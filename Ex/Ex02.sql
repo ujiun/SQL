fbcc10b7e0134e52e6606722993178e8c9edb1f6
ujiun <ujiun@hanmail.net>
2022-05-16 14:23:10 +0900

    그룹함수

--- Ex/Ex02.sql
+++ Ex/Ex02.sql
@@ -95,3 +95,50 @@ and department_id = 90
 and hire_date >= '04/01/01';   /*having 절에는 그룹함수 와 
                                  Group by에 참여한 컬럼만 사용할 수 있다.
                                 */
+
+
+--CASE ~ END 문
+select  employee_id,
+        first_name,
+        job_id,
+        salary,
+        case when job_id = 'AC_ACCOUNT' then salary+salary*0.1
+             when job_id = 'SA_REP' then salary+salary*0.2
+             when job_id = 'ST_CLERK' then salary+salary*0.3
+             else salary
+        END realSalary
+
+from employees;
+
+
+
+
+--DECODE 문
+select  employee_id,
+        first_name,
+        job_id,
+        salary,
+        decode( job_id, 'AC_ACCOUNT', salary+salary*0.1, 
+                        'SA_REP', salary+salary*0.2,
+                        'ST_CLERK', salary+salary*0.3 ,  
+                salary ) bonus
+from employees;
+
+--직원의 이름, 부서, 팀을 출력하세요
+--팀은 코드로 결정하며 
+--부서코드가 10~50 이면 ‘A-TEAM’
+--          60~100이면 ‘B-TEAM’  
+--          110~150이면 ‘C-TEAM’ 
+--          나머지는 ‘팀없음’ 
+--으로 출력하세요.
+select  first_name,
+        department_id,
+        case when department_id>=10 and department_id<=50 then 'A팀'
+             when department_id>=60 and department_id<=100 then 'B팀'
+             when department_id>=110 and department_id<=150 then 'C팀'
+             else '팀없음'
+        end team
+from employees;
+
+
+
