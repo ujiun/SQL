a3bf75cbe2e992a9c54f35ad2cc5c87b83b36764
ujiun <ujiun@hanmail.net>
2022-05-18 01:34:20 +0900

    Practice04

--- Practice/Practice04.sql
+++ Practice/Practice04.sql
@@ -1,158 +1,118 @@
-테이블간 조인(JOIN) SQL 문제입니다.
-
-문제1.
-직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을 조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세요.
-(106건)
-select  em.employee_id,
-        em.first_name,
-        em.last_name,
-        de.department_name
-from employees em, departments de
-where em.department_id = de.department_id
-order by department_name asc, employee_id desc;
-
-
-
-문제2.
-employees 테이블의 job_id는 현재의 업무아이디를 가지고 있습니다.
-직원들의 사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 현재업무(job_title)를 사번(employee_id) 오름차순 으로 정렬하세요.
-부서가 없는 Kimberely(사번 178)은 표시하지 않습니다.
-(106건)
-select  em.employee_id,
-        em.first_name,
-        em.salary,
-        de.department_name,
-        jo.job_title
-from employees em, departments de, jobs jo
-where em.job_id = jo.job_id
-and em.department_id = de.department_id
-order by employee_id asc;
-
-
-문제2-1.
-문제2에서 부서가 없는 Kimberely(사번 178)까지 표시해 보세요
-(107건)
-select  em.employee_id,
-        em.first_name,
-        em.salary,
-        de.department_name,
-        jo.job_title
-from employees em, departments de, jobs jo
-where em.job_id = jo.job_id
-and em.department_id = de.department_id(+)
-order by employee_id asc;
+/*문제1.
+평균 급여보다 적은 급여을 받는 직원은 몇명인지 구하시요.
+(56건)
+*/
+select count(salary)
+from employees
+where salary < (select avg(salary)
+                from employees);
+        
+/*
+문제2. 
+평균급여 이상, 최대급여 이하의 월급을 받는 사원의 
+직원번호(employee_id), 이름(first_name), 급여(salary), 평균급여, 최대급여를 급여의 오름차순으로 정렬하여 출력하세요 
+(51건)
+*/
+select  e.employee_id "직원번호",
+        e.first_name "이름",
+        e.salary "급여",
+        s.avgs "평균급여",
+        s.maxs "최대급여"
+from employees e, (select avg(salary) avgs,
+                          max(salary) maxs
+                   from employees) s
+where e.salary >= s.avgs
+and e.salary <= s.maxs
+order by salary asc;
 
 
 
+/*
 문제3.
-도시별로 위치한 부서들을 파악하려고 합니다.
-도시아이디, 도시명, 부서명, 부서아이디를 도시아이디(오름차순)로 정렬하여 출력하세요 
-부서가 없는 도시는 표시하지 않습니다.
-(27건)
-select  lo.location_id,
-        lo.city,
-        de.department_name,
-        de.department_id
-from locations lo, departments de
-where lo.location_id = de.location_id
-order by location_id asc;
+직원중 Steven(first_name) king(last_name)이 소속된 부서(departments)가 있는 곳의 주소를 알아보려고 한다.
+도시아이디(location_id), 거리명(street_address), 우편번호(postal_code), 도시명(city), 주(state_province), 나라아이디(country_id) 를 출력하세요
+(1건)
+ */
+select l.location_id "도시아이디",
+       l.street_address "거리명",
+       l.postal_code "우편번호",
+       l.city "도시명",
+       l.state_province "주",
+       l.country_id "나라아이디"
+from locations l, departments d
+where l.location_id = d.location_id
+and d.department_id = (select department_id
+                       from employees
+                       where first_name = 'Steven'
+                       and last_name = 'King');
 
-
-문제3-1.
-문제3에서 부서가 없는 도시도 표시합니다. 
-(43건)
-select  lo.location_id,
-        lo.city,
-        de.department_name,
-        de.department_id
-from locations lo, departments de
-where lo.location_id = de.location_id(+)
-order by location_id asc;
-
-
+/*
 문제4.
-지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력하되 지역이름(오름차순), 나라이름(내림차순) 으로 정렬하세요.
-(25건)
-select  re.region_name,
-        co.country_name
-from countries co, regions re
-where co.region_id = re.region_id
-order by region_name asc,
-         country_name desc
+job_id 가 'ST_MAN' 인 직원의 급여보다 작은 직원의 사번,이름,급여를 급여의 내림차순으로 출력하세요  -ANY연산자 사용
+(74건)
+*/
 
+select employee_id "사번",
+       first_name "이름",
+       salary "급여"
+from employees
+where salary < any (select salary
+                    from employees
+                    where job_id = 'ST_MAN')
+order by salary desc;
 
-
+/*
 문제5. 
-자신의 매니저보다 채용일(hire_date)이 빠른 사원의 
-사번(employee_id), 이름(first_name)과 채용일(hire_date), 매니저이름(first_name), 매니저입사일(hire_date)을 조회하세요.
-(37건)
-select  e.employee_id,
-        e.first_name,
-        e.hire_date,
-        m.first_name,
-        m.hire_date
-from employees e, employees m
-where e.manager_id = m.employee_id 
-and e.hire_date < m.hire_date;
-
-
-
-문제6.
-나라별로 어떠한 부서들이 위치하고 있는지 파악하려고 합니다.
-나라명, 나라아이디, 도시명, 도시아이디, 부서명, 부서아이디를 나라명(오름차순)로 정렬하여 출력하세요.
-값이 없는 경우 표시하지 않습니다.
-(27건)
-select  co.country_name,
-        co.country_id,
-        lo.city,
-        lo.location_id,
-        de.department_name,
-        de.department_id
-from departments de, locations lo, countries co
-where de.location_id = lo.location_id
-and lo.country_id = co.country_id
-order by country_id asc;
-
-
-
-문제7.
-job_history 테이블은 과거의 담당업무의 데이터를 가지고 있다.
-과거의 업무아이디(job_id)가 ‘AC_ACCOUNT’로 근무한 사원의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일을 출력하세요.
-이름은 first_name과 last_name을 합쳐 출력합니다.
-(2건)
-select  em.employee_id,
-        em.first_name || ' ' || em.last_name,
-        jo.job_id,
-        jo.start_date,
-        jo.end_date             
-from employees em, job_history jo
-where em.employee_id = jo.employee_id
-and jo.job_id = 'AC_ACCOUNT';
-
-
-
-
-문제8.
-각 부서(department)에 대해서 부서번호(department_id), 부서이름(department_name), 
-매니저(manager)의 이름(first_name), 위치(locations)한 도시(city), 나라(countries)의 이름(countries_name) 그리고 지역구분(regions)의 이름(resion_name)까지 전부 출력해 보세요.
+각 부서별로 최고의 급여를 받는 사원의 직원번호(employee_id), 이름(first_name)과 급여(salary) 부서번호(department_id)를 조회하세요 
+단 조회결과는 급여의 내림차순으로 정렬되어 나타나야 합니다. 
+조건절비교, 테이블조인 2가지 방법으로 작성하세요
 (11건)
-select  de.department_id,
-        de.department_name,
-        em.first_name,
-        lo.city,
-        co.country_name,
-        re.region_name
-from employees em, departments de, locations lo, countries co, regions re
-where em.employee_id = de.manager_id
-and de.location_id = lo.location_id
-and lo.country_id = co.country_id
-and co.region_id = re.region_id;
+*/
+--조건절비교
+
+select  employee_id "직원번호",
+        first_name "이름",
+        salary "급여",
+        department_id "부서번호"
+from employees
+where (salary, department_id) in (select max(salary), department_id
+                                  from employees
+                                  group by department_id)
+order by salary desc;
+
+--테이블조인
+
+select  e.employee_id "직원번호",
+        e.first_name "이름",
+        e.salary "급여",
+        e.department_id "부서번호"
+from employees e, (select max(salary) salary, department_id
+                   from employees
+                   group by department_id) s
+where e.salary = s.salary
+and e.department_id = s.department_id
+order by e.salary desc;
+
+/*
+문제6.
+각 업무(job) 별로 연봉(salary)의 총합을 구하고자 합니다. 
+연봉 총합이 가장 높은 업무부터 업무명(job_title)과 연봉 총합을 조회하시오 
+(19건)
+*/
 
 
 
-문제9.
-각 사원(employee)에 대해서 사번(employee_id), 이름(first_name), 부서명(department_name), 매니저(manager)의 이름(first_name)을 조회하세요.
-부서가 없는 직원(Kimberely)도 표시합니다.
-(106명)
+/*
+문제7.
+자신의 부서 평균 급여보다 연봉(salary)이 많은 직원의 직원번호(employee_id), 이름(first_name)과 급여(salary)을 조회하세요 
+(38건)
+*/
+
+
+
+/*
+문제8.
+직원 입사일이 11번째에서 15번째의 직원의 사번, 이름, 급여, 입사일을 입사일 순서로 출력하세요
+*/
 
 
