0b7f256ac5028e2e38120f763b1905ebb0102eca
ujiun <ujiun@hanmail.net>
2022-05-22 19:28:34 +0900

    practice03

--- Practice/Practice03.sql
+++ Practice/Practice03.sql
@@ -6,7 +6,7 @@
 문제1.
 직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을 조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세요.
 (106건)
-*/
+*/ 
 select  em.employee_id,
         em.first_name,
         em.last_name,
